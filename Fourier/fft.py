515f74dc7db4297feb5bae31270d28d3f15a5f04
aburousan <60939745+aburousan@users.noreply.github.com>
2020-05-28 09:43:09 +0530

    Update fft.py

--- Fourier/fft.py
+++ Fourier/fft.py
@@ -1,29 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-N = 100#No of sample points
-T = 1/N#Sampling Interval
-sigma = 0.5 ;t0=2
-begin_t = 0; end_t = 10;
-t_set = np.linspace(begin_t,end_t,N)
+N = 1000#No of sample points
+T = 100#Time period
+sigma = 1 ;t0=50
+t_set = np.linspace(0,T,N)
+
+an_vel = (2*np.pi)/T #Angular Velocity , I have calculated it at the begining for simplicity
 #__________________________________
-def func(t,t0=2,sigma=0.5):
-	return np.exp(-abs((t-t0)**2)/(2*sigma**2))
+def func(t,t0=50,sigma=1):
+	global an_vel
+	y = np.exp(-abs(an_vel*(t-t0)**2)/(2*sigma**2))
+	#y = np.sin(an_vel*t*2)+np.cos((3*t-t0)*an_vel)
+	return y
 
 plot, dif = plt.subplots(2,1)#Represent 1 row, 2 column
 #__________________________________
-dif[0].plot(t_set,func(t_set),c='red',label='Sigma=%s and t0=%d'%(sigma,t0))
+func_val = func(t_set)
+dif[0].plot(t_set,func_val,c='red',label='Sigma=%s and t0=%d'%(sigma,t0))
 dif[0].legend(loc='best')
 dif[0].set_title(r'Gaussian Function')
 dif[0].set_xlabel('Time'); dif[0].set_ylabel('Function')
 #_______________________________________________________
-ff_t = np.fft.fftshift(func(t_set))/len(t_set)#Normalize
-f_count = len(ff_t)
-time_period = f_count/N
-f_se = np.linspace(0,end_t,N)
-f_set = f_se/time_period
+fre_set = np.fft.fftfreq(N)#All the frequency for fourier transform horizontal axis
+mask = fre_set>0
+fft_vals = 2*np.abs(np.fft.fft(func_val)/len(fre_set))
 
-dif[1].plot(f_set,abs(ff_t),c='green')
+dif[1].plot(fre_set[mask],fft_vals[mask],c='green')
+#dif[1].plot(fre_set,fft_vals,c='green')
+#In quantum domain we work with negative frequency so for that just remove mask i.e., and line 29 and comment in line 30
 dif[1].set_title('Fast Fourier Transform')
 dif[1].set_xlabel('Frequency'); dif[1].set_ylabel('Transform G')
 plt.show()
