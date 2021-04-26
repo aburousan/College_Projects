c5ca3de570170e80bf2582c8112c51795156a66d
Kazi Abu Rousan <60939745+aburousan@users.noreply.github.com>
2021-04-26 11:51:14 +0530

    Update Gaussian_prime_spiral.py

--- Number_Theory/Gaussian_prime_spiral.py
+++ Number_Theory/Gaussian_prime_spiral.py
@@ -1,9 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sympy import isprime
+from sympy import isprime#use this to check if a number is ordinary prime or not
 
-
-def gprime(z):#check if z is gaussian prime or not, return true or false based upon it
+def gprime(z):#check if z is gaussian prime or not, return true or false
 	re = int(abs(z.real)); im = int(abs(z.imag))
 	if re==0 and isprime(im)==True:
 		if im % 4 == 3:
@@ -23,31 +22,28 @@ def gprime(z):#check if z is gaussian prime or not, return true or false based u
 
 seed = [-2,11]#Initial point
 direct = 1# direction of increment
-d = seed[0]+1j*seed[1]; d0=d
+d = seed[0]+1j*seed[1]; d0=d; s = 1
 plot_x = [seed[0]]; plot_y = [seed[1]]; primes = [[d.real],[d.imag]]
-while True:
-	d = direct + d
+while True:#Replace this line by for i in range(100): for 100 steps
+# for i in range(1000):
+	d = direct + d; s+=1
 	plot_x.append(int(d.real)); plot_y.append(int(d.imag))
-	if (seed[0]+1j*seed[1])==d:
+	if (seed[0]+1j*seed[1])==d:#if you use for i in range, then delete this
+	#if statement and break
 		break
 	if gprime(d):
 		direct = direct*1j
 		primes[0].append(d.real);primes[1].append(d.imag)
 
 
-#x1 = np.ma.masked_array(plot_x[:-1], np.diff(plot_x)>=0)
-#x2 = np.ma.masked_array(plot_x[:-1], np.diff(plot_x)<=0)
-
-
 plt.axhline(0,color='Black');plt.axvline(0,color='Black')
-plt.plot(plot_x,plot_y,label='Gaussian spiral',color='mediumblue')
-# plt.plot(x1, plot_y[:-1],'k<',linewidth=1,markevery=20)
-# plt.plot(x2, plot_y[:-1],'k>',linewidth=1,markevery=20)
-plt.scatter(primes[0][0],primes[1][0],c='Black',marker='X')
-plt.scatter(primes[0][1::],primes[1][1::],c='Red',marker='*',label='Gaussian primes')
+plt.plot(plot_x,plot_y,label='Gaussian spiral',color='mediumblue',linewidth=0.1)
+# plt.scatter(plot_x,plot_y,c='Black',label='Gaussian integers')
+plt.scatter(primes[0][0],primes[1][0],c='Black',marker='X')#starting point
+plt.scatter(primes[0][1::],primes[1][1::],c='Red',marker='*',label='Gaussian primes')#,s=0.0001)
 plt.grid(color='purple',linestyle='--')
 plt.legend(loc='best',prop={'size':6})
-plt.xlabel("x ; starting point = %s"%d0)
+plt.xlabel("x ; starting point = %s and loop number = %s "%(d0,s))
 plt.ylabel("y")
-plt.title("Gaussian Integers and Primes spiral")
+# plt.title("Gaussian Primes spiral")
 plt.show()
