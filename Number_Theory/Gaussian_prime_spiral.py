7ff4e4f79e7df49824bd6b2d8df0ed97dc590188
Kazi Abu Rousan <60939745+aburousan@users.noreply.github.com>
2021-05-16 16:00:17 +0530

    Compile faster than pervious one

--- Number_Theory/Gaussian_prime_spiral.py
+++ Number_Theory/Gaussian_prime_spiral.py
@@ -18,23 +18,26 @@ def gprime(z):#check if z is gaussian prime or not, return true or false
 
 def gaussian_spiral(seed, loop_num = 1, del_c = 1, initial_con = True):#Initial condition is actually the fact
 #that represnet if you want to get back to the initial number(seed) at the end.
-	d = seed; points = np.array([seed]); gaussian_primes = np.array([])#Seed is the initial point; points hold all generated gaussian integers
+	d = seed; gaussian_primes_y = []; gaussian_primes_x = []
+	points_x = [d.real]; points_y = [d.imag]
 	if initial_con:
 		while True:
-			seed += del_c
-			points = np.append(points,seed)
+			seed += del_c; real = seed.real; imagi = seed.imag
+			points_x.append(real); points_y.append(imagi)
 			if seed == d:
 				break
 			if gprime(seed):
-				del_c *= 1j ; gaussian_primes = np.append(gaussian_primes,seed)
+				del_c *= 1j
+				gaussian_primes_x.append(real); gaussian_primes_y.append(imagi)
 	else:
 		for i in range(loop_num):
-			seed += del_c
-			points = np.append(points,seed)
+			seed += del_c; real = seed.real; imagi = seed.imag
+			points_x.append(real); points_y.append(imagi)
 			if gprime(seed):
-				del_c *= 1j ; gaussian_primes = np.append(gaussian_primes,seed)
-	points_x = points.real; points_y = points.imag; gauss_primes = np.array([gaussian_primes.real,gaussian_primes.imag])
-	return points_x, points_y, gauss_primes
+				del_c *= 1j ;
+				gaussian_primes_x.append(real); gaussian_primes_y.append(imagi)
+	gauss_p = [gaussian_primes_x,gaussian_primes_y]
+	return points_x, points_y, gauss_p
 
 seed1 = 5 + 23*1j #first plot
 plot_x, plot_y, primes= gaussian_spiral(seed1)
@@ -51,7 +54,7 @@ plt.xlabel("Re(z) ; starting point = %s and loop number = %s "%(seed1,loop_no))
 plt.ylabel("Im(z)")
 # plt.title("Gaussian Primes spiral")
 plt.savefig("img1_new.pdf", bbox_inches = 'tight',dpi = 300)
-#___________________________________________________________________________________________________________-
+##_________________________________________________________________________________________________________________
 # seed2 = 60 + 1j
 # plot_x, plot_y, primes= gaussian_spiral(seed2)
 # loop_no = len(plot_x)-1
@@ -66,16 +69,16 @@ plt.savefig("img1_new.pdf", bbox_inches = 'tight',dpi = 300)
 # plt.xlabel("Re(z); starting point = %s and loop number = %s "%(seed2,loop_no))
 # plt.ylabel("Im(z)")
 # # plt.title("Gaussian Primes spiral")
-# # plt.show()
+# plt.show()
 # plt.savefig("img4_new.pdf", bbox_inches = 'tight',dpi = 300)
-#_________________________________________________________________________________________-
-#seed3 = 277 + 232*1j # This one can kill your computer. Try at your own risk!!
+##_________________________________________________________________________________________________________________
+# seed3 = 277 + 232*1j #This one takes 2min to save the pdf in 300 dpi. So, wait a little.
 # plot_x, plot_y, primes= gaussian_spiral(seed3)
 # loop_no = len(plot_x)-1
 # plt.ylim(-4000,4000)
 # plt.xlim(-4000,4000)
 # plt.axhline(0,color='Black');plt.axvline(0,color='Black')
-# plt.plot(plot_x,plot_y,label='Gaussian spiral',color='mediumblue',bbox_inches = 'tight')
+# plt.plot(plot_x,plot_y,label='Gaussian spiral',color='mediumblue',linewidth=0.1)#,bbox_inches = 'tight')
 # plt.scatter(primes[0][0],primes[1][0],c='Black',marker='X')#starting point
 # plt.scatter(primes[0][1::],primes[1][1::],c='Red',marker='*',label='Gaussian primes',s=0.0001)
 # plt.grid(color='purple',linestyle='--')
@@ -83,8 +86,10 @@ plt.savefig("img1_new.pdf", bbox_inches = 'tight',dpi = 300)
 # plt.xlabel("Re(z); starting point = %s and loop number = %s "%(seed3,loop_no))
 # plt.ylabel("Im(z)")
 # # plt.title("Gaussian Primes spiral")
-# plt.show()
-#_____________________________________________________________________________
+# # plt.show()
+# plt.savefig("img5_new.pdf", bbox_inches = 'tight',dpi = 300)
+
+##_________________________________________________________________________________________________________________
 # seed4 = 3 + 2*1j
 # plot_x, plot_y, primes = gaussian_spiral(seed4,loop_num=30, initial_con = False)
 # loop_no = len(plot_x)-1
@@ -102,3 +107,4 @@ plt.savefig("img1_new.pdf", bbox_inches = 'tight',dpi = 300)
 # # plt.title("Gaussian Primes spiral")
 # # plt.show()
 # plt.savefig("img3_new.pdf", bbox_inches = 'tight',dpi = 300)
+
